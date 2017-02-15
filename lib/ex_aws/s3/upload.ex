1214c4a2beb0080f266f508137c9d47a45344581
azhi <astzhe@gmail.com>
2017-02-15 21:35:01 +0300

    also disable flow for uploads

--- lib/ex_aws/s3/upload.ex
+++ lib/ex_aws/s3/upload.ex
@@ -83,8 +83,8 @@ defimpl ExAws.Operation, for: ExAws.S3.Upload do
     op = Upload.initialize!(op, config)
 
     op.src
-    |> build_producer(op.opts)
-    |> Flow.map(&Upload.upload_chunk!(&1, op, config))
+    |> Stream.with_index(1)
+    |> Stream.map(&Upload.upload_chunk!(&1, op, config))
     |> Enum.to_list
     |> Upload.complete!(op, config)
 
@@ -92,16 +92,4 @@ defimpl ExAws.Operation, for: ExAws.S3.Upload do
   end
 
   def stream!(_, _), do: raise "not implemented"
-
-  defp build_producer(%Flow{} = flow, _opts) do
-    flow
-  end
-  defp build_producer(source, opts) do
-    source
-    |> Stream.with_index(1)
-    |> Flow.from_enumerable(
-          stages: Keyword.get(opts, :max_concurrency, 4),
-          max_demand: 2
-        )
-  end
 end
